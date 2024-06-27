b2ec47414485bc008952fa520bf5ca5de0ac0cfe
Mayuri473 <mayuriraval473@gmail.com>
2024-06-27 19:21:27 +0530

    Made necessary changes.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,20 +1,20 @@
-(function($) {
+(function ($) {
     'use strict';
-    
-     // Mean Menu JS
-	  jQuery('.mean-menu').meanmenu({ 
-		    meanScreenWidth: "991"
+
+    // Mean Menu JS
+    jQuery('.mean-menu').meanmenu({
+        meanScreenWidth: "991"
     });
 
-     // Navbar Area
-    $(window).on('scroll', function() {
-      if ($(this).scrollTop() >150){  
-          $('.navbar-area').addClass("sticky-nav");
-      }
-      else{
-          $('.navbar-area').removeClass("sticky-nav");
-      }
-  });
+    // Navbar Area
+    $(window).on('scroll', function () {
+        if ($(this).scrollTop() > 150) {
+            $('.navbar-area').addClass("sticky-nav");
+        }
+        else {
+            $('.navbar-area').removeClass("sticky-nav");
+        }
+    });
 
     // Banner Into Slider 
     $('.banner-into-slider').owlCarousel({
@@ -24,11 +24,11 @@
         dots: true,
         autoplay: true,
         autoplayHoverPause: true,
-        responsive:{
-            0:{
+        responsive: {
+            0: {
                 items: 2
             },
-            1000:{
+            1000: {
                 items: 3
             }
         }
@@ -43,31 +43,31 @@
         autoplay: true,
         autoplayHoverPause: true,
         navText: [
-          "<i class='bx bxs-chevron-left'></i>",
-          "<i class='bx bxs-chevron-right'></i>"
+            "<i class='bx bxs-chevron-left'></i>",
+            "<i class='bx bxs-chevron-right'></i>"
         ],
-        responsive:{
-            0:{
-              items: 1
+        responsive: {
+            0: {
+                items: 1
             },
-            768:{
+            768: {
                 items: 2
             },
-            1000:{
-              items: 3
+            1000: {
+                items: 3
             }
         }
 
     })
 
     // Tabs
-    $('#tabs-item li a').on('click', function(e) {
+    $('#tabs-item li a').on('click', function (e) {
         $('#tabs-item li, #prices-content .active').removeClass('active').removeClass('fadeInUp');
         $(this).parent().addClass('active');
         var activeTab = $(this).attr('href');
-        $( activeTab).addClass('active fadeInUp');
+        $(activeTab).addClass('active fadeInUp');
         e.preventDefault();
-    });	
+    });
 
     // Testimonial Slider 
     $('.testimonial-slider').owlCarousel({
@@ -79,8 +79,8 @@
         autoplay: true,
         autoplayHoverPause: true,
         navText: [
-          "<i class='bx bxs-chevron-left'></i>",
-          "<i class='bx bxs-chevron-right'></i>"
+            "<i class='bx bxs-chevron-left'></i>",
+            "<i class='bx bxs-chevron-right'></i>"
         ],
     })
 
@@ -94,8 +94,8 @@
         autoplay: true,
         autoplayHoverPause: true,
         navText: [
-          "<i class='bx bxs-chevron-left'></i>",
-          "<i class='bx bxs-chevron-right'></i>"
+            "<i class='bx bxs-chevron-left'></i>",
+            "<i class='bx bxs-chevron-right'></i>"
         ],
     })
 
@@ -108,14 +108,14 @@
         autoplay: true,
         autoplayHoverPause: true,
         navText: [
-          "<i class='bx bx-left-arrow-alt'></i>",
-          "<i class='bx bx-right-arrow-alt' ></i>"
+            "<i class='bx bx-left-arrow-alt'></i>",
+            "<i class='bx bx-right-arrow-alt' ></i>"
         ],
-        responsive:{
-            0:{
+        responsive: {
+            0: {
                 items: 2
             },
-            1000:{
+            1000: {
                 items: 4
             }
         }
@@ -131,16 +131,16 @@
         autoplay: true,
         autoplayHoverPause: true,
         navText: [
-          "<i class='bx bxs-chevron-left'></i>",
-          "<i class='bx bxs-chevron-right'></i>"
+            "<i class='bx bxs-chevron-left'></i>",
+            "<i class='bx bxs-chevron-right'></i>"
         ],
     })
 
     // Tabs Single Page
     $('.tab ul.tabs').addClass('active').find('> li:eq(0)').addClass('current');
     $('.tab ul.tabs li a').on('click', function (g) {
-        var tab = $(this).closest('.tab'), 
-        index = $(this).closest('li').index();
+        var tab = $(this).closest('.tab'),
+            index = $(this).closest('li').index();
         tab.find('ul.tabs > li').removeClass('current');
         $(this).closest('li').addClass('current');
         tab.find('.tab_content').find('div.tabs_item').not('div.tabs_item:eq(' + index + ')').slideUp();
@@ -158,57 +158,57 @@
         autoplay: true,
         autoplayHoverPause: true,
         navText: [
-          "<i class='bx bxs-chevron-left'></i>",
-          "<i class='bx bxs-chevron-right'></i>"
+            "<i class='bx bxs-chevron-left'></i>",
+            "<i class='bx bxs-chevron-right'></i>"
         ],
     })
 
     // FAQ Accordion JS
-	$('.accordion').find('.accordion-title').on('click', function(){
-		// Adds Active Class
-		$(this).toggleClass('active');
-		// Expand or Collapse This Panel
-		$(this).next().slideToggle('fast');
-		// Hide The Other Panels
-		$('.accordion-content').not($(this).next()).slideUp('fast');
-		// Removes Active Class From Other Titles
-		$('.accordion-title').not($(this)).removeClass('active');		
+    $('.accordion').find('.accordion-title').on('click', function () {
+        // Adds Active Class
+        $(this).toggleClass('active');
+        // Expand or Collapse This Panel
+        $(this).next().slideToggle('fast');
+        // Hide The Other Panels
+        $('.accordion-content').not($(this).next()).slideUp('fast');
+        // Removes Active Class From Other Titles
+        $('.accordion-title').not($(this)).removeClass('active');
     });
-    
+
     // Count Time JS
-	function makeTimer() {
-		var endTime = new Date("july  30, 2025 17:00:00 PDT");			
-		var endTime = (Date.parse(endTime)) / 1000;
-		var now = new Date();
-		var now = (Date.parse(now) / 1000);
-		var timeLeft = endTime - now;
-		var days = Math.floor(timeLeft / 86400); 
-		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
-		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
-		if (hours < "10") { hours = "0" + hours; }
-		if (minutes < "10") { minutes = "0" + minutes; }
-		if (seconds < "10") { seconds = "0" + seconds; }
-		$("#days").html(days + "<span>Days</span>");
-		$("#hours").html(hours + "<span>Hours</span>");
-		$("#minutes").html(minutes + "<span>Minutes</span>");
-		$("#seconds").html(seconds + "<span>Seconds</span>");
-	}
-	setInterval(function() { makeTimer(); }, 300);
+    function makeTimer() {
+        var endTime = new Date("july  30, 2025 17:00:00 PDT");
+        var endTime = (Date.parse(endTime)) / 1000;
+        var now = new Date();
+        var now = (Date.parse(now) / 1000);
+        var timeLeft = endTime - now;
+        var days = Math.floor(timeLeft / 86400);
+        var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+        var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
+        var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+        if (hours < "10") { hours = "0" + hours; }
+        if (minutes < "10") { minutes = "0" + minutes; }
+        if (seconds < "10") { seconds = "0" + seconds; }
+        $("#days").html(days + "<span>Days</span>");
+        $("#hours").html(hours + "<span>Hours</span>");
+        $("#minutes").html(minutes + "<span>Minutes</span>");
+        $("#seconds").html(seconds + "<span>Seconds</span>");
+    }
+    setInterval(function () { makeTimer(); }, 300);
 
     // Nice Select JS
     $('select').niceSelect();
-   
+
     // Input Plus & Minus Number JS
-    $('.input-counter').each(function() {
+    $('.input-counter').each(function () {
         var spinner = jQuery(this),
-        input = spinner.find('input[type="text"]'),
-        btnUp = spinner.find('.plus-btn'),
-        btnDown = spinner.find('.minus-btn'),
-        min = input.attr('min'),
-        max = input.attr('max');
-        
-        btnUp.on('click', function() {
+            input = spinner.find('input[type="text"]'),
+            btnUp = spinner.find('.plus-btn'),
+            btnDown = spinner.find('.minus-btn'),
+            min = input.attr('min'),
+            max = input.attr('max');
+
+        btnUp.on('click', function () {
             var oldValue = parseFloat(input.val());
             if (oldValue >= max) {
                 var newVal = oldValue;
@@ -219,7 +219,7 @@
             spinner.find("input").trigger("change");
         });
 
-        btnDown.on('click', function() {
+        btnDown.on('click', function () {
             var oldValue = parseFloat(input.val());
             if (oldValue <= min) {
                 var newVal = oldValue;
@@ -229,7 +229,7 @@
             spinner.find("input").val(newVal);
             spinner.find("input").trigger("change");
         });
-    }); 
+    });
 
     // Subscribe form
     $(".newsletter-form").validator().on("submit", function (event) {
@@ -242,7 +242,7 @@
             event.preventDefault();
         }
     });
-    function callbackFunction (resp) {
+    function callbackFunction(resp) {
         if (resp.result === "success") {
             formSuccessSub();
         }
@@ -250,28 +250,28 @@
             formErrorSub();
         }
     }
-    function formSuccessSub(){
+    function formSuccessSub() {
         $(".newsletter-form")[0].reset();
         submitMSGSub(true, "Thank you for subscribing!");
-        setTimeout(function() {
+        setTimeout(function () {
             $("#validator-newsletter").addClass('hide');
         }, 4000)
     }
-    function formErrorSub(){
+    function formErrorSub() {
         $(".newsletter-form").addClass("animated shake");
-        setTimeout(function() {
+        setTimeout(function () {
             $(".newsletter-form").removeClass("animated shake");
         }, 1000)
     }
-    function submitMSGSub(valid, msg){
-        if(valid){
+    function submitMSGSub(valid, msg) {
+        if (valid) {
             var msgClasses = "validation-success";
         } else {
             var msgClasses = "validation-danger";
         }
         $("#validator-newsletter").removeClass().addClass(msgClasses).text(msg);
     }
-        
+
     // AJAX MailChimp
     $(".newsletter-form").ajaxChimp({
         url: "https://hibootstrap.us20.list-manage.com/subscribe/post?u=60e1ffe2e8a68ce1204cd39a5&amp;id=42d6d188d9", // Your url MailChimp
@@ -280,20 +280,20 @@
 
     // Back To Top Js
     $('body').append('<div id="toTop" class="top-btn"><i class="bx bx-chevrons-up"></i></div>');
-    $(window).on('scroll',function () {
+    $(window).on('scroll', function () {
         if ($(this).scrollTop() != 0) {
             $('#toTop').fadeIn();
         } else {
             $('#toTop').fadeOut();
         }
-    }); 
-    $('#toTop').on('click',function(){
+    });
+    $('#toTop').on('click', function () {
         $("html, body").animate({ scrollTop: 0 }, 0);
         return false;
     });
 
     // Preloader JS
-    jQuery(window).on('load',function(){
+    jQuery(window).on('load', function () {
         jQuery(".preloader").fadeOut(500);
     });
 
@@ -305,21 +305,32 @@ function setTheme(themeName) {
     document.documentElement.className = themeName;
 }
 
-// Gallery section
+// Gallery section Js
 
 const modal = document.getElementsByClassName('idMyModal');
 const img = document.getElementsByClassName('toZoom');
 const modalImg = document.getElementsByClassName('modal-content');
-for ( let i = 0; i < img.length; i++ ) {
-  img[i].onclick = function () {
-    modal[i].style.display = "block";
-    modalImg[i].src = this.src;
-  }
+for (let i = 0; i < img.length; i++) {
+    img[i].onclick = function () {
+        modal[i].style.display = "block";
+        modalImg[i].src = this.src;
+    }
 }
 
 var span = document.getElementsByClassName("close");
-for ( let i = 0; i < span.length; i++ ) {
-  span[i].onclick = function() { 
-    modal[i].style.display = "none";
-  }
+for (let i = 0; i < span.length; i++) {
+    span[i].onclick = function () {
+        modal[i].style.display = "none";
+    }
+}
+
+// Product Img Js
+
+const productImg = document.getElementById("ProductImg");
+const smallImg = document.getElementsByClassName("small-img");
+
+for (let i = 0; i < smallImg.length; i++) {
+    smallImg[i].onclick = function () {
+        productImg.src = smallImg[i].src;
+    }
 }
